4cfa844c23c1638e4f0ede7d3e42eb90198ebcc9
Gennadii <kruglov.gennadii@gmail.com>
2020-07-23 21:40:41 +0300

    add model Note

---
+++ ManagementNotesApp/Models/Note.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagementNotesApp.Models
+{
+    public class Note
+    {
+        /// <summary>
+        /// The unique Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The note text
+        /// </summary>
+        public string TextNote { get; set; }
+    }
+}
